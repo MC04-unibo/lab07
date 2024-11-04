9a1d3a3e0f445d30ea4eacdc0485eab38229f026
MC04-unibo <michele.casamenti@studio.unibo.it>
2024-11-04 12:45:30 +0100

    completed Part 2: iterable with policy

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableImpl.java
@@ -6,43 +6,77 @@ import it.unibo.inner.api.IterableWithPolicy;
 import it.unibo.inner.api.Predicate;
 
 public class IterableImpl<T> implements IterableWithPolicy<T>{
-    T[] array;
+    private T[] array;
+    private Predicate<T> predicate;
 
     public IterableImpl(T[] array){
         this.array = array;
+        this.predicate = new Predicate<T>() {
+            @Override
+            public boolean test(T elem) {
+                return true;    
+            }
+        };
+    }
+
+    public IterableImpl(T[] array, Predicate<T> predicate){
+        this.array = array;
+        this.predicate = predicate;
     }
 
     public Iterator<T> iterator(){
-        return new IteratorImpl(array);
+        return new IteratorImpl(array,predicate);
     }
 
     @Override
     public void setIterationPolicy(Predicate<T> filter) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+        this.predicate = filter;
     }
 
     class IteratorImpl implements Iterator<T>{
         private int current;
         private T[] array;
-        IteratorImpl(T[] array){
+        private Predicate<T> predicate;
+
+        public IteratorImpl(T[] array, Predicate<T> predicate){
             this.current = 0;
             this.array = array;
+            this.predicate = predicate;
         }
 
         @Override
         public boolean hasNext() {
-            return array.length != current && array.length > 0;
+            return elementHasNext(current);
+        }
+
+        private boolean elementHasNext(int elem){
+            if(elem == array.length){
+                return false;
+            }
+
+            if(predicate.test(array[elem])){
+                return true;
+            }else{
+                return elementHasNext(elem+1);
+            }
         }
 
         @Override
         public T next() {
+            if(current >= array.length){
+                return null;
+            }
+
             if(array != null && array.length > 0){
                 if(this.hasNext()){
                     current++;
-                    return array[current-1];
+                    if(predicate.test(array[current-1])){
+                        return array[current-1];
+                    }else{
+                        return next();
+                    }
                 }else{
-                    return array[current];
+                    return null;
                 }
             }else{
                 return null;
